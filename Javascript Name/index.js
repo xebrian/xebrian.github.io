816cd9289ab6371ac15b161141169292abcec7c8
xebrian <70643451+xebrian@users.noreply.github.com>
2022-06-24 11:13:58 +0100

    Add files via upload

---
+++ Javascript Name/index.js
@@ -0,0 +1,6 @@
+let Name = 'Charles Onochie';
+var Height = '6ft';
+var Country = 'Nigeria';
+
+alert('Name: ' + Name + "  " + 'Height: ' + Height + " " + 'Country: '+ Country);
+document.write('Name: ' + Name + " <br/> " + 'Height: ' + Height + " <br/> " + 'Country: '+ Country);
